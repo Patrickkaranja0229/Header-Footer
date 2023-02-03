144df0f1e3b730c8cfbaec8197a7b2781a440337
patrickkaranja0229 <patrickkaranja0229@gmail.com>
2023-02-03 08:51:54 +0300

    mme

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div>
+           <p>Footer goes here</p>
+      <span>
+        <button>Back to the top</button>
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
